c6905315c86a5acc818a2139812b87847336591e
kcharish <charishma910@gmail.com>
2025-05-22 17:57:19 +0530

    roboshop_scripts

--- redis.sh
+++ redis.sh
@@ -1,60 +1,56 @@
 #!/bin/bash
 
 ID=$(id -u)
-
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
 
-TIMESTAMP=$(date +%F-%H-%M-%s)
+TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 exec &>$LOGFILE
 
-echo "script Started executing at $TIMESTAMP" &>> $LOGFILE
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
-    if [ $1 ne 0 ]
+    if [ $1 -ne 0 ]
     then
-        echo -e "$2.... $R FAILED $N"
+        echo -e "$2 ... $R FAILED $N"
         exit 1
     else
-        echo -e "$2.... $G SUCCESS $N"
-    fi   
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
 }
+
 if [ $ID -ne 0 ]
-then 
-    echo "$R ERROR:: Please run this script with root access $N"
-
-    exit 1 # we can give other than 0
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
 else
-    echo "you are a root user"
-fi
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
 
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.5.rpm -y
 
-VALIDATE $? "installing remi release" 
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 
-dnf module reset redis -y
-
-VALIDATE $? "resetting redis" 
+VALIDATE $? "Installing Remi release"
 
 dnf module enable redis:remi-6.2 -y
 
-VALIDATE $? "enabling remi v6.2" 
+VALIDATE $? "enabling redis"
 
 dnf install redis -y
 
-VALIDATE $? "installing redis" 
+VALIDATE $? "Installing Redis"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
 
-VALIDATE $? "allowing Remote access"
+VALIDATE $? "allowing remote connections"
 
 systemctl enable redis
 
-VALIDATE $? "enabling redis" 
+VALIDATE $? "Enabled Redis"
 
 systemctl start redis
 
-VALIDATE $? "starting redis" 
-
+VALIDATE $? "Started Redis"

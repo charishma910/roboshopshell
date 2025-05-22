b9fd4c1fab285fcba0ad24381909dc6de84e9a20
kcharish <charishma910@gmail.com>
2025-05-22 11:52:55 +0530

    roboshop_scripts

--- roboshop.sh
+++ roboshop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-AMI=ami567799767uy788
-SG_ID=sg-087e7afb3a936fce7    #replace with my security group ID
+AMI=ami-0b4f379183e5706b9
+SG_ID=sg-06386db686aed9498    #replace with my security group ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-ZONE_ID=66666 # need to copy from route53
+ZONE_ID=Z1018280I2IQGUYPUVNP # need to copy from route53
 DOMAIN_NAME=daws76s.shop
 for i in "${INSTANCES[@]}"
 do
@@ -15,8 +15,8 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a07781880afb --instance-type 
-    $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query "Instances[0].PrivateIpAddress" --output text)
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type 
+    $INSTANCE_TYPE --security-group-ids sg-06386db686aed9498 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query "Instances[0].PrivateIpAddress" --output text)
 
     echo "$i: $IP_ADDRESS"
     # creating route53 record make sure deleting exisisting record
